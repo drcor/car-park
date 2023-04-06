bc4fd40354dd5689c8d9e4ad00931624ef2fed7b
cardosoox <96376944+cardosoox@users.noreply.github.com>
2023-04-06 12:43:08 +0100

    Login v1

---
+++ login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>LOGIN</title>
+</head>
+<body>
+    <div class="main-login">
+        <div class="left-login">
+            <h1>Faz o login</h1> 
+            <h1>O melhor Car Park da zona</h1>
+        </div>
+        <div class="right-login">
+            <div class="card">
+                <h1> LOGIN</h1>
+                <div class="text-field">
+                    <label for="user"> Utilizador: </label>
+                    <input type="text" name="user" >
+                </div>
+                <div class="text-field">
+                    <label for="user"> Password: </label>
+                    <input type="password" name="user" >
+                </div>
+                <button class="btn-login">Login</button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>

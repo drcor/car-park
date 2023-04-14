8a6cf7b0880e9d4948bde903c4e14aa0a3fbcd5f
cardosoox <96376944+cardosoox@users.noreply.github.com>
2023-04-14 09:00:06 +0100

    bootstrap update

--- login.php
+++ login.php
@@ -1,33 +1,40 @@
 <!DOCTYPE html>
-<html lang="pt">
+<html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>Parque de Estacionamento</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css?<?php echo time(); ?>">
-    
-    <title>LOGIN</title>
+
+    <!-- Bootstrap v5.1.3 CDNs -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- CSS File -->
+    <link rel="stylesheet" href="style.css">
+
 </head>
+
 <body>
-    <div class="main-login">
-        <div class="left-login">
-            <h1>Faz o login</h1> 
-            <h1>O melhor Car Park da zona</h1>
-        </div>
-        <div class="right-login">
-            <div class="card">
-                <h1> LOGIN</h1>
-                <div class="text-field">
-                    <label for="user"> Utilizador: </label>
-                    <input type="text" name="user" >
+    <div class="login">
+        <h1 class="text-center">Bem-Vindo!</h1>
+        <form class="needs-validation">
+            <div class="form-group was-validated">
+                <label class="form-label" for="email">Email</label>
+                <input class="form-control" type="email" id="email" required>
+                <div class="invalid-feedback">
+                    Escreva o seu endereço de email
                 </div>
-                <div class="text-field">
-                    <label for="user"> Password: </label>
-                    <input type="password" name="user" >
-                </div>
-                <button class="btn-login">Login</button>
             </div>
-        </div>
+            <div class="form-group was-validated">
+                <label class="form-label" for="password">Password</label>
+                <input class="form-control" type="password" id="password" required>
+                <div class="invalid-feedback">
+                    Escreva a sua password
+                </div>
+            </div>
+            <input class="btn btn-primary w-100" type="submit" value="SIGN IN">
+        </form>
     </div>
 </body>
 </html>

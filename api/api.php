c06b15cbbd25698484c1fab08ed47fc360664e3f
Diogo Correia <53583855+drcor@users.noreply.github.com>
2023-04-15 02:22:23 +0100

    Add actuators files and adapt API

--- api/api.php
+++ api/api.php
@@ -2,14 +2,19 @@
 include_once('../utils.php');
 
 header('Content-Type: text/html; charset=UTF-8');
-$options = get_dirs('files/');
+
+$files = get_dirs('files/');
 
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	if (isset($_POST['nome']) and in_array($_POST['nome'], $options) and (isset($_POST['valor']) and !empty($_POST['valor'])) and (isset($_POST['hora']) and !empty($_POST['hora']))) {
+	if (isset($_POST['nome'])
+		and in_array($_POST['nome'], $files)						// Verifica se o nome do sensor/atuador existe
+		and ( isset($_POST['valor']) and !empty($_POST['valor']) )		// Verifica se o valor e a hora estão atribuidos e não são nulos
+		and ( isset($_POST['hora']) and !empty($_POST['hora']) )) {
+		
 		file_put_contents('files/' . $_POST['nome'] . '/valor.txt', $_POST['valor']);
 		file_put_contents('files/' . $_POST['nome'] . '/hora.txt', $_POST['hora']);
-		file_put_contents('files/' . $_POST['nome'] . '/log.txt', PHP_EOL . $_POST['hora'] . ';' . $_POST['valor'], FILE_APPEND);
+		file_put_contents('files/' . $_POST['nome'] . '/log.txt', $_POST['hora'] . ';' . $_POST['valor'].PHP_EOL, FILE_APPEND);
 	} else {
 		echo "Faltam parametros no POST";
 		http_response_code(400);

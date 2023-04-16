a3caf8a1aa1b8957cdc2f789bf51c6ef19c0d26d
cardosoox <96376944+cardosoox@users.noreply.github.com>
2023-04-16 10:53:21 +0100

    update session
    a

--- login.php
+++ login.php
@@ -1,16 +1,25 @@
+<?php
+session_start();
+
+$username="admin";    
+$password_hash='$2y$10$WhpkqfSmVl3l9ev1jLr0OeG07kvrPcT4R0FQUzOG3tXP11nvHYdAO'; //pass: admin
+
+if (isset($_POST['username']) and isset($_POST['password'])) {
+    if (password_verify($_POST['password'], $password_hash)) {
+        $_SESSION["username"] = $_POST['username'];
+        header("refresh:1;url=dashboard.php");
+    }
+}
+
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt">
 
 <head>
-
     <title>Parque de Estacionamento</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- Bootstrap v5.1.3 CDNs -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-
-    <!-- CSS File -->
     <link rel="stylesheet" href="style.css">
 
 </head>
@@ -18,22 +27,22 @@
 <body>
     <div class="login">
         <h1 class="text-center">Bem-Vindo!</h1>
-        <form class="needs-validation">
+        <form  method="post" class="needs-validation">
             <div class="form-group was-validated">
-                <label class="form-label" for="email">Email</label>
-                <input class="form-control" type="email" id="email" required>
+                <label class="form-label" for="email">Username</label>
+                <input class="form-control" type="username" name="username" required>
                 <div class="invalid-feedback">
                     Escreva o seu endereço de email
                 </div>
             </div>
             <div class="form-group was-validated">
                 <label class="form-label" for="password">Password</label>
-                <input class="form-control" type="password" id="password" required>
+                <input class="form-control" type="password" name="password" required>
                 <div class="invalid-feedback">
                     Escreva a sua password
                 </div>
             </div>
-            <input class="btn btn-primary w-100" type="submit" value="SIGN IN">
+            <input class="btn btn-primary w-100" type="submit" value="ENTRA">
         </form>
     </div>
 </body>

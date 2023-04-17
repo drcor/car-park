2a978f7909aa9eb1bce77074d09691b4dcad5558
Diogo Correia <53583855+drcor@users.noreply.github.com>
2023-04-17 11:47:16 +0100

    Update project folder structure
    Add wallpaper to login

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ if (isset($_POST['username']) and isset($_POST['password'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles/style.css">
 
 </head>
 

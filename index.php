9c12dcfcd5a9393ff868ab9ba9257e1cfdcee641
Diogo Correia <53583855+drcor@users.noreply.github.com>
2023-04-21 18:43:34 +0100

    Add link to Roboto font

--- index.php
+++ index.php
@@ -28,17 +28,20 @@
         }
     }
 ?>
-
 <!DOCTYPE html>
 <html lang="pt">
 <head>
     <title>Parque de Estacionamento</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- ficheiro css-->
-    <link rel="stylesheet" href="styles/login.css?<?php echo time(); ?>">
-   <!-- bootstrap -->
+    <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Font Roboto -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <!-- ficheiro css-->
+    <link rel="stylesheet" href="styles/login.css">
 </head>
 <body>
     <!-- Card Login -->

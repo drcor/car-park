4d7167ee99a17a64da659772689964462ac0516e
Diogo Correia <53583855+drcor@users.noreply.github.com>
2023-05-29 22:58:58 +0100

    Update dashboard to update content without refresh
    Pass dashboard.js code to dashboard.php so that it can be changed
    directely with php.
    Update API to send information of confirmation.

--- api/api.php
+++ api/api.php
@@ -5,6 +5,7 @@ header('Content-Type: text/html; charset=UTF-8');
 
 $files = get_dirs('files/');
 
+// Método POST
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['nome'])
         and in_array($_POST['nome'], $files)                        // Verifica se o nome do sensor/atuador existe
@@ -17,18 +18,29 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         file_put_contents('files/' . $_POST['nome'] . '/valor.txt', $_POST['valor']);
         file_put_contents('files/' . $_POST['nome'] . '/hora.txt', $date);
         file_put_contents('files/' . $_POST['nome'] . '/log.txt', $date . ';' . $_POST['valor'].PHP_EOL, FILE_APPEND);
+
+        $nome = file_get_contents('files/'. $_POST['nome'] . '/nome.txt');
+        $info = file_get_contents('files/'. $_POST['nome'] . '/info.txt');
+        
+        echo $_POST['nome'].';' . $nome . ';' . $info;
+
     } else {
         echo "Faltam parametros no POST";
         http_response_code(400);
     }
-} else if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+} else if ($_SERVER['REQUEST_METHOD'] == 'GET') {   // Método GET
     if (isset($_GET['nome']) and in_array($_GET['nome'], $files)) {
-        echo file_get_contents('files/' . $_GET['nome'] . '/valor.txt');
+        // Obter específicamente a data da última atualização
+        if (isset($_GET['tipo']) and $_GET['tipo'] == "hora") {
+            echo file_get_contents('files/'. $_GET['nome'] . '/hora.txt');
+        } else {    // Senão retorna normalmente o valor do sensor/atuador
+            echo file_get_contents('files/' . $_GET['nome'] . '/valor.txt');
+        }
     } else {
         echo "Faltam parametros no GET";
         http_response_code(400);
     }
-} else {
+} else {    // Qualquer outro método
     echo 'Metodo nao permitido';
     http_response_code(403);
 }
--- dashboard.php
+++ dashboard.php
@@ -1,21 +1,21 @@
 <?php
-    require_once('utils.php');
+require_once('utils.php');
 
-    session_start();
+session_start();
 
-    // Obtem credencias do ficheiro
-    $credentials = analyze_credentials('../credentials.txt');
+// Obtem credencias do ficheiro
+$credentials = analyze_credentials('../credentials.txt');
 
-    // Verifica se algum utilizador já se encontra logado
-    if (!isset($_SESSION['username']) or !is_user($_SESSION['username'], $credentials)) {
-        header("Location: index.php");
-    }
+// Verifica se algum utilizador já se encontra logado
+if (!isset($_SESSION['username']) or !is_user($_SESSION['username'], $credentials)) {
+    header("Location: index.php");
+}
 
-    // Obter crecenciais referentes ao utilizador logado
-    $user = get_user($_SESSION['username'], $credentials);
+// Obter crecenciais referentes ao utilizador logado
+$user = get_user($_SESSION['username'], $credentials);
 
-    // Obter lista de sensores/atuadores
-    $files = get_dirs('api/files/');
+// Obter lista de sensores/atuadores
+$files = get_dirs('api/files/');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -27,8 +27,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
     <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-    <!-- Font Roboto -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.0/dist/jquery.min.js"></script>
+    <!-- Import Robot Font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
@@ -76,7 +76,12 @@
 
                         // Configura automaticamente o nome da imagem para cada sensor/atuador
                         $primeiro_nome = explode(' ', $nome)[0];
+                        $switch = '';
                         if ($info == 'atuador') {
+                            $switch = '<div class="form-check form-switch">
+                                            <label for="' . $file . '-switch">Off/On</label>
+                                            <input id="' . $file . '-switch" name="' . $file . '" type="checkbox" role="switch" class="form-check-input" ' . ($valor == 'On' ? 'checked' : '') . '>
+                                        </div>';
                             $imagem = strtolower($primeiro_nome . '_' . $valor);
                         } else {
                             $imagem = strtolower($primeiro_nome);
@@ -85,11 +90,12 @@
                         echo '<div class="col-sm-4">
                                 <div class="card text-center mb-3">
                                     <div class="card-header fw-bold sensor">' . $nome . '</div>
-                                    <img src="images/' . $imagem . '.png" alt="' . $nome . '" class="card-image-top">
+                                    <img id="' . $file . '-image" src="images/' . $imagem . '.png" alt="' . $nome . '" class="card-image-top">
                                     <div class="card-body">
                                         <h5 class="card-title mb-3"><span id="' . $file . '">' . $valor . '</span> ' . get_sensor_symbol($nome) . '</h5>
-                                        <small><b>Ultima atualização:</b> ' . $hora . '</small>
-                                        ' . ($user[2] == 'admin' ? "<a href=\"historico.php?nome=$file\" class=\"text-primary\">Histórico</a>" : '') . '
+                                        <small><b>Última atualização:</b> <span id="' . $file . '-hora">' . $hora . '</span></small>
+                                        ' . ($user[2] == 'admin' ? "<a href=\"historico.php?nome=$file\" class=\"text-primary\">Histórico</a>" : '') .
+                            $switch . '
                                     </div>
                                 </div>
                             </div>';
@@ -146,8 +152,111 @@
             let sidebar = document.getElementById('sidebar');
             sidebar.classList.toggle('active');
         });
+
+        function updateFromAPI(id, nome, isAtuador = false) {
+            // console.log("Updated " + id + "");
+
+            // Obtem a data e hora da ultima atualização do sensor/atuador na API
+            let spanHora = document.getElementById(id + "-hora");
+            const getHora = async () => {
+                const response = await fetch('/api/api.php?nome=' + id + "&tipo=hora");
+                // check if the response is ok
+                if (response.ok) {
+                    const textValue = await response.text();
+                    // console.log(textValue)
+                    spanHora.innerHTML = textValue; // Atualiza a hora na pagina
+                }
+            }
+            getHora().catch(error => console.error(error));
+
+            // Obtem o valor do sensor/atuador
+            let spanValor = document.getElementById(id)
+            const getValor = async () => {
+                const response = await fetch('/api/api.php?nome=' + id);
+                // check if the response is ok
+                if (response.ok) {
+                    const textValue = await response.text();
+                    // console.log(textValue)
+                    spanValor.innerHTML = textValue; // Atualiza o estado na pagina
+
+                    // Atualiza o botão switch caso seja um atuador
+                    let inputSwitch = document.getElementById(id + '-switch');
+                    if (isAtuador) {
+                        if (textValue === "On") {
+                            inputSwitch.checked = true;
+                        } else {
+                            inputSwitch.checked = false;
+                        }
+                    }
+
+                    // Atualiza a imagem
+                    console.log("Valor: " + textValue)
+
+                    // Cria o caminha para obter a imagem
+                    let imageName = '';
+                    let primeiroNome = nome.split(" ")[0];
+                    if (isAtuador && textValue != null) { // Configura o nome caso seja um atuador
+                        imageName = (primeiroNome + "_" + textValue).toLowerCase();
+                    } else { // Ou caso seja um sensor
+                        imageName = primeiroNome.toLowerCase();
+                    }
+                    // Obtem a imagem correspondente ao sensor/atuador
+                    const image = document.getElementById(id + "-image");
+                    fetch("/images/" + imageName + ".png")
+                        .then((response) => response.blob())
+                        .then((blob) => {
+                            const objectURL = URL.createObjectURL(blob);
+                            image.src = objectURL;
+                        });
+                }
+            }
+            getValor().catch(error => console.error(error));
+        }
+
+        // Define um evento de click para todos os botões switch
+        document.querySelectorAll('.form-check-input').forEach(element => {
+            element.addEventListener('click', async function() {
+                let value = 'Off';
+
+                // Se o switch for ativado define o valor a enviar como ligado
+                if (this.checked) {
+                    value = 'On';
+                }
+                // Envia o POST para atualizar o estado do atuador através da API
+                const sendPost = async () => {
+                    const response = await fetch('api/api.php', {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/x-www-form-urlencoded",
+                        },
+                        body: "nome=" + this.name + "&valor=" + value,
+                    });
+
+                    if (response.ok) {
+                        const textValue = await response.text();
+                        const valores = textValue.split(';');
+                        console.log(textValue + ' - ' + valores);
+
+                        updateFromAPI(valores[0], valores[1], valores[2]);
+                    } else {
+                        // Caso ocorra algum erro retorna o botão switch ao estado original
+                        this.checked = false;
+                    }
+                }
+                sendPost().catch(error => console.error(error));
+            });
+        });
+
+        <?php
+        foreach ($files as $file) {
+            $nome = file_get_contents('api/files/' . $file . '/nome.txt');
+            $info = file_get_contents('api/files/' . $file . '/info.txt');
+
+            // Cria um setInterval para cada sensor/atuador
+            echo "setInterval(updateFromAPI, 5000, '" . $file . "', '" . $nome . "', " . ($info == 'atuador' ? 'true' : 'false') . ");\n";
+        }
+        ?>
     </script>
-    <script src="scripts/dashboard.js"></script>
     <!--Popper.JS -->
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <!--Bootstrap JS -->

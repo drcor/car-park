5309a103a5efc50341e260c205cd2fef8aef0720
Diogo Correia <53583855+drcor@users.noreply.github.com>
2023-06-03 16:31:52 +0100

    Fix upload.php file validation fault

--- api/upload.php
+++ api/upload.php
@@ -4,10 +4,9 @@
 	header('Content-Type: text/html; charset=UTF-8');
 	$options = get_dirs_list('files/');
 
-    print_r($_FILES);
-	
+    // print_r($_FILES);
+	// Método POST
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        print_r($_FILES['imagem']);
         if (isset($_FILES['imagem'])                            // Verifica se foi definida alguma imagem como upload
             and $_FILES['imagem']['error'] == 0                 // Verifica se não ocorreu nenhum erro a fazer o upload
             and $_FILES['imagem']['size'] > 0                   // Verifica se a imagem não é nula
@@ -20,14 +19,14 @@
 
             // Valida se o tipo de ficheiro corresponde a uma imagem
             if (!in_array($mimetype, $allowedfiles)) {
-                echo 'Tipo de ficheiro não permitido';
                 http_response_code(400);
+                die('Tipo de ficheiro não permitido');
             }
 
             // Valida se o tamanho do ficheiro carregado é tem o tamanho máximo de 1000kB (1024000 Bytes)
             if ($_FILES['imagem']['size'] > 1024000) {
-                echo 'A imagem excedeu o tamanho máximo permitido';
                 http_response_code(400);
+                die('A imagem excedeu o tamanho máximo permitido');
             }
 
             // Move a atual imagem de webcam para a pasta das imagens antigas com a hora de upload da imagem
@@ -38,11 +37,11 @@
             move_uploaded_file($_FILES['imagem']['tmp_name'], "./images/webcam.jpg");
 
             // Se o histórico atingir as 11 imagens remove a mais antiga
-            $image_files = get_files_list('api/images/older/'); // Obtem a lista do historico de webcam
+            $image_files = get_files_list('images/older/'); // Obtem a lista do historico de webcam
             asort($image_files);                                // e ordena ascendentemente
             print_r($image_files);
-            if (count($image_files) >= 11) {
-                unlink("api/images/older/".$image_files[0]);
+            if (count($image_files) >= 12) {
+                unlink("images/older/".$image_files[0]);
             }
 
 

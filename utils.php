2ceded9d92add83857dd2852843b4140f8c4f51e
Diogo Correia <53583855+drcor@users.noreply.github.com>
2023-06-10 19:21:00 +0100

    Add ventoinha to decrease levels of co2

--- utils.php
+++ utils.php
@@ -35,6 +35,8 @@ function get_sensor_symbol($sensor_name) {
         case 'Humidade':
         case 'CO2':
             return '%';
+        case 'Ventoinha':
+            return 'rps';
         default:
             return '';
     }

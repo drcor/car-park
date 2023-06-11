b79ce5021bf97cc63f3c1bcbe5f084d4167a0894
Diogo Correia <53583855+drcor@users.noreply.github.com>
2023-06-11 22:51:21 +0100

    Fix php time 1 hour ahead

--- api/api.php
+++ api/api.php
@@ -3,6 +3,8 @@ include_once('../utils.php');
 
 header('Content-Type: text/html; charset=UTF-8');
 
+date_default_timezone_set("Europe/Lisbon");
+
 $files = get_dirs_list('files/');
 
 // METODO POST
